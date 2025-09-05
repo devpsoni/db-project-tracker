055ae420c0e684b10b6314a71c35d41ccf8cf566
devpsoni <iamdevsoni@gmail.com>
2025-09-05 11:48:27 +0530

    Initial Commit

---
+++ db-project-tracker/db_project_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  db_project_trackerApp.swift
+//  db-project-tracker
+//
+//  Created by Dev Soni on 05/09/25.
+//
+
+import SwiftUI
+
+@main
+struct db_project_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
